8999a9e1514f7c13e18e887b40dee116bef2a13b
brian-abea <97589095+brian-abea@users.noreply.github.com>
2022-01-12 11:08:22 +0300

    Add files via upload
    Solutions Sacco system is management that manages all tasks that take place in a Sacco, that is opening accounts, savings, and also borrowing of loans.

---
+++ src/sacco/Account.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sacco;
+
+/**
+ *
+ * @author sn
+ */
+public class Account {
+    private double interest;
+    private int accountNumber;
+    private static int numberOfAccounts = 1000000;
+    
+    Account(){
+        accountNumber = numberOfAccounts++;
+    }
+    /**
+     * @return the interest
+     */
+    public double getInterest() {
+        return interest;
+    }
+
+    /**
+     * @param interest the interest to set
+     */
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    /**
+     * @return the accountNumber
+     */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @param accountNumber the accountNumber to set
+     */
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    
+    
+}
---
+++ src/sacco/DBConnection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sacco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sn
+ */
+public class DBConnection {
+     Connection con;
+    PreparedStatement pst;
+    ResultSet rs = null;
+
+    public DBConnection() throws SQLException {
+        this.con = DriverManager.getConnection("jdbc:mysql://localhost/sacco","root","");
+    }
+}
